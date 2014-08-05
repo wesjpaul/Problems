cd1cf291fa6b07ea087b6ffa6c60ead25cc61dff
Wesley Paul <wesjpaul@gmail.com>
2014-08-05 12:06:49 -0700

    First Commit for Problems Project

---
+++ src/Problems/Main.java
@@ -0,0 +1,11 @@
+package Problems;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Sum of even fibonnaci numbers: " + Fibonacci.RunFibonacci());
+		System.out.println("Largest palindrome in string: " + Palindrome.RunPalindrome("asdjfhjadfjadfjfdjhqwertrewqasdfdsaflk"));
+		System.out.println("Largest palindome in 3 digit products: " + PalindromicNumbers.RunPalindromicNumbers());
+	}
+
+}
---
+++ src/Problems/Palindrome.java
@@ -0,0 +1,17 @@
+package Problems;
+
+/*
+ *	Find the largest palendrome in a string.
+ */
+
+public class Palindrome {
+
+	public static String RunPalindrome(String text){
+		String palindrome = new String();
+		palindrome = text;
+		
+		
+		return palindrome;
+	}
+	
+}
