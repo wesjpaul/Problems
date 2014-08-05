3a245d07ad802e3eeb46b43a654d2210d84fa0eb
Wesley Paul <wesjpaul@gmail.com>
2014-08-05 13:39:45 -0700

    Updated Comments

--- src/Problems/Fibonacci.java
+++ src/Problems/Fibonacci.java
@@ -16,6 +16,8 @@ package Problems;
  *	
  *	By considering the terms in the Fibonacci sequence whose values do not exceed four million, 
  *	find the sum of the even-valued terms.
+ *
+ *	Source: https://projecteuler.net/
  */
 
 public class Fibonacci {
--- src/Problems/PalindromicNumbers.java
+++ src/Problems/PalindromicNumbers.java
@@ -12,6 +12,8 @@ package Problems;
  *	A palindromic number reads the same both ways. The largest palindrome made 
  *	from the product of two 2-digit numbers is 9009 = 91 × 99.
  *	Find the largest palindrome made from the product of two 3-digit numbers.
+ *
+ *	Source: https://projecteuler.net/
  */
 
 public class PalindromicNumbers {
