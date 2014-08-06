efe84fe6adb72e8f7b83274f8d9208de33de077c
Wesley Paul <wesjpaul@gmail.com>
2014-08-06 14:33:48 -0700

    3 Digit Palindromic number completed

--- src/Problems/PalindromicNumbers.java
+++ src/Problems/PalindromicNumbers.java
@@ -1,10 +1,8 @@
 /*
  *  Author: Wesley Paul
- *  Date: 
+ *  Date: August 06, 2014
  * 
  */
-
-
 
 package Problems;
 
@@ -16,14 +14,46 @@ package Problems;
  *	Source: https://projecteuler.net/
  */
 
+
 public class PalindromicNumbers {
 	
+	private static final int MAXVALUE = 999;
+	private static final int MINVALUE = 99;
+	
+	private static boolean palindrome(int num){
+		String number = String.valueOf(num);
+		String reverse = new StringBuilder(number).reverse().toString();
+		
+		return number.equals(reverse);
+		
+	}
 	
 	public static int RunPalindromicNumbers(){
+		int i = 0, j = 0;
+		int largest = 0;
 		
+		// Multiply all 3 digit values together starting from MAXVALUE 
+		// and iterating down
 		
+		for(i=MAXVALUE; i > MINVALUE; i--){
+			// if the current iteration multiplied by MAXVALUE is less than the largest found 
+			// palindrome then we are done.
+			if(i*MAXVALUE <= largest)
+				break;
+			
+			for(j=MAXVALUE; j > MINVALUE; j--){
+				// if the current product iteration is less than the largest known palindrome
+				// then we are done with this value of i
+				if(i*j <= largest)
+					break;
+				// if a palindrome is found it must be the largest so far.
+				if(palindrome(i*j))
+					largest = i*j;
+				
+			}
+		}
 		
-		return 0;
+		return largest;
 	}
 	
 	
